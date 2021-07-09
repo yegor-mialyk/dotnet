08938ebd8150d6f29b95838cb59c28d2d811867d
Yegor Mialyk <16239726+yegor-mialyk@users.noreply.github.com>
2021-07-09 12:42:58 -0700

    Added string extensions

---
+++ My.Common/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace My.Common
+{
+    public static class StringExtensions
+    {
+        public static int ToInt32(this string? s, int defaultValue = 0)
+        {
+            return int.TryParse(s, out var result) ? result : defaultValue;
+        }
+
+        public static long ToInt64(this string? s, long defaultValue = 0)
+        {
+            return long.TryParse(s, out var result) ? result : defaultValue;
+        }
+
+        public static bool IsNullOrEmpty([NotNullWhen(false)] this string? value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+    }
+}
