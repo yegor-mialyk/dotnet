d19a3051a5d11c99c4830d6a27b6f4ddb8fbf4a3
Yegor Mialyk <16239726+yegor-mialyk@users.noreply.github.com>
2023-04-19 17:23:00 -0700

    Added type extensions

---
+++ My.Common/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿//
+// Type Extensions
+//
+// Copyright (C) 1995-2023, Yegor Mialyk. All Rights Reserved.
+//
+// Licensed under the MIT License. See the LICENSE file for details.
+//
+
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace My.Common;
+
+public static class TypeExtensions
+{
+    public static bool IsNullableTypeOf<T>(this Type type)
+    {
+        return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) &&
+               type.GetGenericArguments()[0] == typeof(T);
+    }
+
+    public static IDictionary<string, string> ToDictionary<T>(this T type)
+    {
+        return typeof(T).GetFields(BindingFlags.Static | BindingFlags.Public)
+            .ToDictionary(field => field.GetValue(type)?.ToString() ?? string.Empty,
+                field => field.GetCustomAttributes<DisplayAttribute>(false).FirstOrDefault()?.Name ?? field.Name);
+    }
+}
